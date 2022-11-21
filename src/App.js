143483f79693cbfa5fe61f355c5902a8947d0626
madcow95 <lobasketve@naver.com>
2022-11-21 19:40:28 +0900

    import, export

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import dataUtil from "./util/data";
+import compUtil from "./util/component"
+import { useState } from 'react';
+import { Container, Nav, Navbar, Row, Col } from 'react-bootstrap';
 
 function App() {
+
+  const [ product, setProduct ] = useState( dataUtil.productData );
+
+  /* 내 현재 절대경로 : {process.env.PUBLIC_URL} => 이미지 경로 설정할때 사용 */
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { compUtil.getHeader() }
+      <div className='main-bg'></div>
+      <Container>
+        <Row>
+          { product.map( ( p, idx ) => <Card ProductState={ p } ProductIndex={ idx } key={ idx }/> ) }
+        </Row>
+      </Container>
     </div>
   );
+}
+
+const Card = ( props ) => {
+  const value = props.ProductState;
+  const index = props.ProductIndex;
+  return (
+    <Col>
+      <img src={ `https://codingapple1.github.io/shop/shoes${ String( index + 1 ) }.jpg` } width="80%"/>
+      <h4>{ value.title }</h4>
+      <p>{ value.content }</p>
+      <p>{ value.price }</p>
+    </Col>
+  )
 }
 
 export default App;
