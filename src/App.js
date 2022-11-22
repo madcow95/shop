0f4cd9b63c58e3022408a263e065478be86e8cfd
madcow95 <lobasketve@naver.com>
2022-11-22 21:14:06 +0900

    Navigate사용

--- src/App.js
+++ src/App.js
@@ -2,36 +2,56 @@ import './App.css';
 import dataUtil from "./util/data";
 import compUtil from "./util/component"
 import { useState } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+// import { Container, Row, Col } from 'react-bootstrap';
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
 
-function App() {
+// npm install react-router-dom@6 설치
+const App = () => {
 
   const [ product, setProduct ] = useState( dataUtil.getProductData() );
+  // use로 시작하는 애들은 유용한애들이 들어있는 함수 => Hook이라고 함
+  // 페이지 이동을 도와주는 아이
+  const navigate = useNavigate();
 
-  /* 내 현재 절대경로 : {process.env.PUBLIC_URL} => 이미지 경로 설정할때 사용 */
   return (
     <div className="App">
-      { compUtil.getHeader() }
+      { compUtil.getHeader( navigate ) }
       <div className='main-bg'></div>
-      <Container>
-        <Row>
-          { product.map( ( p, idx ) => <Card ProductState={ p } ProductIndex={ idx } key={ idx }/> ) }
-        </Row>
-      </Container>
+
+      {/* Route = 페이지라고 생각하면 됨 */}
+      <Routes>
+        <Route path="/" element={ compUtil.getMainPage( product, navigate ) } />
+        <Route path="/detail" element={ compUtil.getProductDetail( navigate ) } />
+
+        <Route path="/event" element={ <Event/> }>
+          <Route path='one' element={ <p>첫 주문시 양배추즘 서비스</p> }/>
+          <Route path='two' element={ <p>생일 쿠폰 받기</p> }/>
+        </Route>
+
+        {
+        /* 
+        Nested Route
+        여러 유사한 페이지 필요할 때 이용하면 좋음ㄹ
+        /about/location으로 들어왔을 때 이런식으로도 작성 가능
+        대신 About component의 return안에 <Outlet></Outlet>으로 표현 가능 
+        <Route path="/about" element={<About/>}>
+          <Route path="/location" element={<Location/>}/>
+        </Route> */
+        }
+
+        {/* 위에 적은 경로 외의 다른 이상한 페이지로 접근시 아래로 향함 */}
+        <Route path="/*" element={ <div>잘못 들어오신거 같은데요?</div> } />
+      </Routes>
     </div>
   );
 }
 
-const Card = ( props ) => {
-  const value = props.ProductState;
-  const index = props.ProductIndex;
+const Event = () => {
   return (
-    <Col>
-      <img src={ `https://codingapple1.github.io/shop/shoes${ String( index + 1 ) }.jpg` } width="80%" alt=''/>
-      <h4>{ value.title }</h4>
-      <p>{ value.content }</p>
-      <p>{ value.price }</p>
-    </Col>
+    <>
+      <h1>오늘의 이벤트</h1>
+      <Outlet></Outlet>
+    </>
   )
 }
 
--- src/util/component.js
+++ src/util/component.js
@@ -1,20 +1,65 @@
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar, Row, Col } from 'react-bootstrap';
+import { Link } from "react-router-dom";
 
-export const getHeader = () => {
+export const getHeader = ( navigate ) => {
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
                 <Navbar.Brand href="#home">SHOP</Navbar.Brand>
                 <Nav className="me-auto">
-                    <Nav.Link href="#home">홈</Nav.Link>
-                    <Nav.Link href="#Login">로그인</Nav.Link>
-                    <Nav.Link href="#Join">회원가입</Nav.Link>
+                    <Nav.Link onClick={ () => { navigate( '/' ) } }>홈</Nav.Link>
+                    <Nav.Link href="/login">로그인</Nav.Link>
+                    <Nav.Link href="/join">회원가입</Nav.Link>
                 </Nav>
             </Container>
         </Navbar>
     )
 }
 
+export const getMainPage = ( product, navigate ) => {
+    return (
+      <Container>
+        <Row>
+          { product.map( ( p, idx ) => <CardList ProductState={ p } ProductIndex={ idx } Navigate={ navigate } key={ idx }/> ) }
+        </Row>
+      </Container>
+    )
+}
+
+const CardList = ( props ) => {
+    const value = props.ProductState;
+    const index = props.ProductIndex;
+    const navigate = props.Navigate;
+    return (
+      <Col onClick={ () => { navigate( '/detail' ) } }>
+        <img src={ `https://codingapple1.github.io/shop/shoes${ String( index + 1 ) }.jpg` } width="80%" alt=''/>
+        <h4>{ value.title }</h4>
+        <p>{ value.content }</p>
+        <p>{ value.price }</p>
+      </Col>
+    )
+}
+
+export const getProductDetail = ( navigate ) => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6">
+                <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+                </div>
+                <div className="col-md-6">
+                <h4 className="pt-5">상품명</h4>
+                <p>상품설명</p>
+                <p>120000원</p>
+                <button className="btn btn-danger">주문하기</button> 
+                </div>
+            </div>
+        </div>
+    )
+}
+
 export default {
-    getHeader
+    getHeader,
+    getMainPage,
+    getProductDetail
 }
