02e518062c44d9ef5974dadea65dd98c0bc34743
madcow95 <lobasketve@naver.com>
2022-11-29 22:14:09 +0900

    state 간소화

--- src/App.js
+++ src/App.js
@@ -1,31 +1,22 @@
 import './App.css';
-import dataUtil from "./util/data";
 import compUtil from "./util/component";
-import { useState } from 'react';
 import { Routes, Route, useNavigate } from "react-router-dom";
-// import { Container, Row, Col } from 'react-bootstrap';
 
 const App = () => {
-  const [ product, setProduct ] = useState( dataUtil.getProductData() );
+  
   const navigate = useNavigate();
   
   return (
     <div className="App">
-      { compUtil.getHeader( navigate ) }
+      <compUtil.GetHeader NavigateState={ navigate }/>
       <div className='main-bg'></div>
       <Routes>
-        <Route path="/" element={ <compUtil.getMainPage ProductState={ product } MainState={ useState } NavigateState={ navigate } /> } />
-        <Route path="/detail/:id" element={ <compUtil.GetProductDetail ProductState={ product } SetProductState={ setProduct } MainState={ useState } /> } />
-        <Route path="/login" element={ <compUtil.GetLoginPage MainState={ useState }/> } />
-        <Route path="/join" element={ <compUtil.GetJoinPage MainState={ useState }/> } />
+        <Route path="/" element={ <compUtil.GetMainPage NavigateState={ navigate } /> } />
+        <Route path="/detail/:id" element={ <compUtil.GetProductDetail /> } />
+        <Route path="/login" element={ <compUtil.GetLoginPage /> } />
+        <Route path="/join" element={ <compUtil.GetJoinPage /> } />
         <Route path="/*" element={ <div>잘못 들어오신거 같은데요?</div> } />
       </Routes>
-
-      {/* <button onClick={ () => {
-        const copiedProduct = [ ...product ];
-        copiedProduct.sort( ( a, b ) => { return a.title.localeCompare( b.title ) } );
-        setProduct( copiedProduct );
-      } }>Sort!</button> */}
     </div>
   );
 }
