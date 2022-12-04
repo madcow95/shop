48dcd16b7b913ce339a5da4b9cb116b5c8a1dad7
madcow95 <lobasketve@naver.com>
2022-12-04 17:38:04 +0900

    파일이름 변경

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import ctx from "./ctx";
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  <Provider ctx={ ctx }>
+  <Provider store={ store }>
     <BrowserRouter>
       <App />
     </BrowserRouter>
